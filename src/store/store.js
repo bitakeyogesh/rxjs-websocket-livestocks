2df7540cb9de9f9c24dc285c258bf923b483eac7
Bitake <a3znzg@splm.siemens.com>
2021-05-16 20:27:40 +0530

    enable redux dev tool

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,16 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { rootReducer } from "../reducers/rootReducer";
 import { createEpicMiddleware } from "redux-observable";
 import { rootEpic } from "../epics/epics";
 
 const epicMiddleware = createEpicMiddleware();
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const configureStore = () => {
-  const store = createStore(rootReducer, applyMiddleware(epicMiddleware));
+  const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(epicMiddleware))
+  );
 
   epicMiddleware.run(rootEpic);
 
